89112d647bcb57fd76b2a68cc59c8abd0706d247
Ramesh Ramaswamy <rramaswamy@Portal01.nextgen.com>
2017-11-27 11:46:56 +0530

    Nlog Demo
    Added Nlog Demo application

---
+++ NLog Demo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+using NLog.Web;
+
+
+namespace NLog_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        Logger logger = LogManager.GetLogger("NLogDemo");
+
+        public ActionResult Index()
+        {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
